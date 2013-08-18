a1c88d1aa3a800c3ed1c4b2a3cc1c868853c8f7a
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2013-08-18 19:23:42 -0400

    Cleaned some of the graphic generators

--- public/tileGenerator.js
+++ public/tileGenerator.js
@@ -1,7 +1,8 @@
 function createTiles(size){
 
 
-	var tileColors=["rgba(0,0,0,0)","rgba(0,0,0,0)","#AA3339","#00bb33","#3f5eb5","orange"];
+	var tileColors=["rgba(0,0,0,0)","rgba(0,0,0,0)","#AA3339","#00bb33","#3f5ed5","#ffb500"];
+	//var tileColors=["rgba(0,0,0,0)","rgba(0,0,0,0)","red","green","blue","orange"];	
 	
 	var tiles=[];
 	
@@ -13,6 +14,8 @@ function createTiles(size){
 		tile.width=size;
 		tile.height=size;
 		
+		ctx.fillStyle=tileColors[i];
+		ctx.fillRect(0,0,size,size);
 		
 		draw(ctx,i,size);
 	
@@ -22,8 +25,6 @@ function createTiles(size){
 	
 	function draw(ctx,i,size){
 	
-		ctx.fillStyle=tileColors[i];
-		ctx.fillRect(0,0,size,size);
 		
 		ctx.scale(size/100,size/100);
 		switch(i){
@@ -35,76 +36,55 @@ function createTiles(size){
 		break;
 		case(3):
 
-ctx.strokeStyle = 'rgba(0,0,0,0)';
-ctx.lineCap = 'butt';
-ctx.lineJoin = 'miter';
-ctx.miterLimit = 4;
-ctx.save();
-ctx.restore();
-ctx.save();
+
 ctx.translate(0,-952.36218);
-ctx.save();
-//ctx.fillStyle = "#00ff00";
+
 ctx.strokeStyle = "#88FF88";
 ctx.lineWidth = 8;
 ctx.lineCap = "round";
 ctx.lineJoin = "miter";
 ctx.miterLimit = 4;
+
 ctx.beginPath();
 ctx.moveTo(23.829093,984.91159);
-ctx.bezierCurveTo(30.808991,998.54696,41.617135000000005,996.97076,50.783578,987.89926);
-ctx.bezierCurveTo(58.963708,979.80385,72.654305,981.86177,78.24466799999999,992.74971);
-//ctx.fill();
+ctx.bezierCurveTo(30.808991,998.54696,41.617135,996.97076,50.783578,987.89926);
+ctx.bezierCurveTo(58.963708,979.80385,72.654305,981.86177,78.244668,992.74971);
 ctx.stroke();
-//ctx.fillStyle = "rgba(0, 0, 0, 0)";
-//ctx.strokeStyle = "#000000";
-//ctx.lineWidth = 11.100000381469727;
-//ctx.lineCap = "round";
-//ctx.lineJoin = "miter";
-//ctx.miterLimit = 4;
+
 ctx.beginPath();
 ctx.moveTo(24.161064,1008.5132);
-ctx.bezierCurveTo(31.140962000000002,1022.1486,41.949106,1020.5724,51.115549,1011.5009);
+ctx.bezierCurveTo(31.140962,1022.1486,41.949106,1020.5724,51.115549,1011.5009);
 ctx.bezierCurveTo(59.295679,1003.4055,72.986276,1005.4634,78.576639,1016.3514);
-//ctx.fill();
+
 ctx.stroke();
-ctx.restore();
-ctx.restore();
+
 break;
 case(4):
 
-ctx.lineCap = 'butt';
-ctx.lineJoin = 'miter';
-ctx.miterLimit = 4;
-ctx.save();
-ctx.restore();
-ctx.save();
-ctx.translate(0,-952.36218);
-ctx.save();
-ctx.fillStyle = "#7ed3ff";
-ctx.translate(0,952.36218);
+
+
+ctx.fillStyle = "#7ed3dd";
 ctx.beginPath();
 ctx.moveTo(33,22.875);
 ctx.bezierCurveTo(25.175642,22.875,18.84375,29.238142,18.84375,37.0625);
-ctx.bezierCurveTo(18.84375,44.886858000000004,25.175642,51.21875,33,51.21875);
-ctx.bezierCurveTo(40.824358000000004,51.21875,47.15625,44.886858000000004,47.15625,37.0625);
+ctx.bezierCurveTo(18.84375,44.886858,25.175642,51.21875,33,51.21875);
+ctx.bezierCurveTo(40.8243580,51.21875,47.15625,44.886858,47.15625,37.0625);
 ctx.bezierCurveTo(47.15625,29.238142,40.824358,22.875,33,22.875);
 ctx.closePath();
 ctx.moveTo(67,23.09375);
-ctx.bezierCurveTo(59.175641999999996,23.09375,52.84375,29.425642,52.84375,37.25);
-ctx.bezierCurveTo(52.84375,45.074358000000004,59.175641999999996,51.4375,67,51.4375);
-ctx.bezierCurveTo(74.824358,51.4375,81.15625,45.074358000000004,81.15625,37.25);
+ctx.bezierCurveTo(59.175642,23.09375,52.84375,29.425642,52.84375,37.25);
+ctx.bezierCurveTo(52.84375,45.074358,59.175642,51.4375,67,51.4375);
+ctx.bezierCurveTo(74.824358,51.4375,81.15625,45.074358,81.15625,37.25);
 ctx.bezierCurveTo(81.15625,29.425642,74.824358,23.09375,67,23.09375);
 ctx.closePath();
 ctx.moveTo(50,52.53125);
-ctx.bezierCurveTo(42.175641999999996,52.53125,35.84375,58.863141999999996,35.84375,66.6875);
-ctx.bezierCurveTo(35.84375,74.511858,42.175641999999996,80.875,50,80.875);
-ctx.bezierCurveTo(57.824358000000004,80.875,64.15625,74.511858,64.15625,66.6875);
-ctx.bezierCurveTo(64.15625,58.863141999999996,57.824358000000004,52.53125,50,52.53125);
+ctx.bezierCurveTo(42.175642,52.53125,35.84375,58.863142,35.84375,66.6875);
+ctx.bezierCurveTo(35.84375,74.511858,42.175642,80.875,50,80.875);
+ctx.bezierCurveTo(57.824358,80.875,64.15625,74.511858,64.15625,66.6875);
+ctx.bezierCurveTo(64.15625,58.863142,57.824358,52.53125,50,52.53125);
 ctx.closePath();
 ctx.fill();
-ctx.restore();
-ctx.restore();
+
 
 break;
 		case(5):
