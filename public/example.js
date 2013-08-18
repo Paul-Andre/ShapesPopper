eba2ca218bf52b19dbae4fa73aedbb8cbcc7f3c7
Paul-Andre Henegar <chapeaufeutre@gmail.com>
2013-08-17 20:31:53 -0400

    Added stuff inside of public, so public will appear on github.

---
+++ public/example.js
@@ -0,0 +1,12 @@
+// this is just some random javascript.
+
+var x=3;
+
+var paragraph=document.createElement("p")
+paragraph.innerHTML="Hello world, whatever there is here, is whatever... "+x+" times.";
+
+
+window.onload=function(){document.body.appendChild(paragraph);}
+
+//alert("blablablah");
+//lets write stuff in comments. blblblablab
